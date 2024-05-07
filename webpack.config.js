69565f9dccc54b6fb1405bc0db529e4fc6236a90
ZiaKhan2008 <88408107+ZiaKhan2008@users.noreply.github.com>
2024-05-07 16:37:56 -0400

    Carbo Credit files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'development', // Set the mode to development or production
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // Output bundle file
+  },
+  devServer: {
+    host: '0.0.0.0',
+    compress: true,
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,  // Match JavaScript and JSX files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader', // Use Babel loader
+        },
+      },
+      {
+        test: /\.css$/, // Match CSS files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+      },
+    ],
+  },
+  externals: [nodeExternals()], // Exclude Node.js core modules
+};
